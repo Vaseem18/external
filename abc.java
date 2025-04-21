bbe956062ac57a29cdcbede60ead9bebb25e3f61
Vaseem18 <166577021+Vaseem18@users.noreply.github.com>
2025-04-21 05:20:21 +0000

    Update abc.java

--- abc.java
+++ abc.java
@@ -2,5 +2,6 @@ public class abc{
 	public static void main(String args[]){
      System.out.println("It is a Program");
      System.out.println("It is a java");
+     System.out.println("It is a java program");
      }
     }
